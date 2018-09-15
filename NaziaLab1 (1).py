5b6409bda1f749fded1a51dad4fc0dcfe9a7c6a0
NaziaShramin <43231744+NaziaShramin@users.noreply.github.com>
2018-09-15 16:07:01 -0600

    Update NaziaLab1 (1).py

--- NaziaLab1 (1).py
+++ NaziaLab1 (1).py
@@ -14,43 +14,29 @@ def classify_pic(path):
 
     return random.random() / 2
 
-def process_dir(path):
+def process_dir(path, cat_list, dog_list):
     dir_list, file_list = get_dirs_and_files(path)
-    cat_list, dog_list = [], []
+
     # Your code goes here
-    for root, dirs, files in os.walk(path):
-        for file_ in files:
-            if file_[-4:]==".jpg": # if image type, then only consider that
-                if classify_pic(path + file_) < 0.5:
-                    cat_list.append(file_)
-                else:
-                    dog_list.append(file_)
-
-    return cat_list, dog_list
-
-def getListOfFiles(path, cat_list, dog_list):
     listOfFile = os.listdir(path)
-    allFiles = []
     for file in listOfFile:
         fullPath = os.path.join(path, file)
         if os.path.isdir(fullPath):
-            newFileList, cat_list, dog_list = getListOfFiles(fullPath, cat_list, dog_list)
-            allFiles = allFiles + newFileList
+            cat_list, dog_list = process_dir(fullPath, cat_list, dog_list)
         else:
-            allFiles.append(fullPath)
             if file[-4:] == ".jpg": # if image type, then only consider that
                 if classify_pic(path + file) < 0.5:
                     cat_list.append(file)
                 else:
                     dog_list.append(file)
 
-    return allFiles, cat_list, dog_list
+    return cat_list, dog_list
 
 def main():
     start_path = 'C:/Users/Nazia/1.1 CatsDogs/1.1 CatsDogs' # current directory
 
     cat_list, dog_list = [], []
-    newAllfiles, newCatList, newDogList = getListOfFiles(start_path, cat_list, dog_list )
+    cat_list, dog_list = process_dir(start_path, cat_list, dog_list)
 
     print("Dogs list:", dog_list)
     print("Cat list:", cat_list)
